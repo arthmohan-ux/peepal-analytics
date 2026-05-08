fe59bea9fca7d7f8260eb311a9bf6f1919fca1ae
Arth Mohan <arth.mohan@peepalconsulting.com>
2026-05-08 14:20:57 +0530

    pushed

--- netlify/lib/sheets.js
+++ netlify/lib/sheets.js
@@ -1,17 +1,16 @@
 const { google } = require('googleapis');
 
-function getAuth() {
-  const privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
-  return new google.auth.JWT(
-    process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    null,
-    privateKey,
-    ['https://www.googleapis.com/auth/spreadsheets.readonly']
-  );
-}
-
 async function getSheetData(sheetName) {
-  const auth = getAuth();
+  const privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
+
+  const auth = new google.auth.JWT({
+    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    key: privateKey,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  });
+
+  await auth.authorize();
+
   const sheets = google.sheets({ version: 'v4', auth });
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SHEET_ID,
